d193d17deb027d12068f449ccd48d36f14a363bd
Sunnyk16 <kumarsunny1246@gmail.com>
2024-01-10 14:12:55 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import  ReactDOM  from 'react-dom/client';
+import "./index.css"
+import Img1 from './images/img1.png'
+import Img2 from './images/img2.jpeg'
+import Img3 from './images/img3.jpg'
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Cards(props){
+  console.log(props);
+  const name=props.name;
+  const mobile=props.mobile;
+  const img = props.img
+  return(
+    <div className='container'>
+      
+      <div className='cards'>
+        <div className='img'>
+        <img src={img} />
+        </div>
+      <p>
+        Name:{name}
+      </p>
+      <p>
+        mobile:{mobile}
+        </p>
+    </div>
+    </div>
+    
+  )
+}
+
+root.render(
+  <div className='boxes'>
+      <Cards name='sunny' mobile='7896541234' img={Img1}/>
+  <Cards name='om' mobile='7896546634' img={Img2} />
+  <Cards name='sadanand' mobile='783241234' img={Img3} />
+  </div>
+)
