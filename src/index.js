344db9b1bc8859e716f2fd6293ac797e6ba0fb6c
Sunnyk16 <kumarsunny1246@gmail.com>
2024-01-11 01:02:47 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
-import  ReactDOM  from 'react-dom/client';
-import "./index.css"
-import Img1 from './images/img1.png'
-import Img2 from './images/img2.jpeg'
-import Img3 from './images/img3.jpg'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Img1 from "./images/img1.png";
+import Img2 from "./images/img2.jpeg";
+import Img3 from "./images/img3.jpg";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Cards(props){
+function Cards(props) {
   console.log(props);
-  const name=props.name;
-  const mobile=props.mobile;
-  const img = props.img
-  return(
-    <div className='container'>
-      
-      <div className='cards'>
-        <div className='img'>
-        <img src={img} />
+  const name = props.name;
+  const mobile = props.mobile;
+  const img = props.img;
+  return (
+    <div className="container">
+      <div className="cards">
+        <div className="img">
+          <img src={img} />
         </div>
-      <p>
-        Name:{name}
-      </p>
-      <p>
-        mobile:{mobile}
-        </p>
+        <p>Name:{name}</p>
+        <p>mobile:{mobile}</p>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
 root.render(
-  <div className='boxes'>
-      <Cards name='sunny' mobile='7896541234' img={Img1}/>
-  <Cards name='om' mobile='7896546634' img={Img2} />
-  <Cards name='sadanand' mobile='783241234' img={Img3} />
+  <div className="boxes">
+    <Cards name="sunny" mobile="7896541234" img={Img1} />
+    <Cards name="om" mobile="7896546634" img={Img2} />
+    <Cards name="sadanand" mobile="783241234" img={Img3} />
+    <Cards name="vaishanvi" mobile="446541654" img={Img2}/>
   </div>
-)
+);
---
+++ src/index1.js
@@ -0,0 +1,16 @@
+import  ReactDOM  from 'react-dom/client';
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function cards(){
+    return(
+        <div 
+    )
+}
+
+root.render(
+  <div className='boxes'>
+  
+  </div>
+)
